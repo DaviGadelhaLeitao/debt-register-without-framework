d90438d2cd5294ba7e4c97e8e06d46e924e29670
Davi Leitão <davi.leitao@hotmail.com>
2016-12-21 00:20:55 -0300

    Adicionando o teste de integração para alterar um caloteiro no CaloteiroDAOTest.

--- test-integracao/br/com/triadworks/javaweb/dao/test/CaloteiroDAOTest.java
+++ test-integracao/br/com/triadworks/javaweb/dao/test/CaloteiroDAOTest.java
@@ -17,7 +17,7 @@ import br.com.triadworks.javaweb.dao.ConnectionFactory;
 import br.com.triadworks.javaweb.modelo.Caloteiro;
 
 public class CaloteiroDAOTest {
-	
+
 	private Connection conexao;
 
 	@Before
@@ -25,6 +25,7 @@ public class CaloteiroDAOTest {
 		this.conexao = new ConnectionFactory().getConnection();
 		limpaTabelaCaloteiro();
 	}
+
 	@After
 	public void tearDown() {
 		try {
@@ -34,7 +35,7 @@ public class CaloteiroDAOTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void limpaTabelaCaloteiro() {
 		String sql = "delete from caloteiro";
 
@@ -42,13 +43,13 @@ public class CaloteiroDAOTest {
 			PreparedStatement pstmt = conexao.prepareStatement(sql);
 
 			pstmt.execute();
-			pstmt.close();		
+			pstmt.close();
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	private Caloteiro getCaloteiroTeste(String nome) {
 		Caloteiro caloteiro = new Caloteiro();
 		caloteiro.setNome(nome);
@@ -57,47 +58,71 @@ public class CaloteiroDAOTest {
 		caloteiro.setDevendo(300);
 		return caloteiro;
 	}
-	
+
 	@Test
 	public void deveAdicionarCaloteiroNoBanco() {
-		
+
 		Caloteiro caloteiro = getCaloteiroTeste("Joao");
-		
+
 		CaloteiroDAO dao = new CaloteiroDAO();
 		dao.adiciona(caloteiro);
-		
+
 		List<Caloteiro> lista = dao.getLista();
 		String nome = lista.get(0).getNome();
 		assertEquals(caloteiro.getNome(), nome);
 	}
-	
+
 	@Test
 	public void deveDeletarCaloteiroNoBanco() {
-		
-		Caloteiro caloteiro = getCaloteiroTeste("Roberto");
-		
+
+		Caloteiro caloteiro = getCaloteiroTeste("Joao");
+
 		CaloteiroDAO dao = new CaloteiroDAO();
 		dao.adiciona(caloteiro);
-		
+
 		caloteiro.setId(dao.getLista().get(0).getId());
 		dao.deleta(caloteiro);
-		
+
 		List<Caloteiro> lista = dao.getLista();
 		assertEquals(lista.size(), 1);
 	}
-	
+
 	@Test
 	public void deveRetornarUmCaloteiro() {
 		Caloteiro caloteiro = getCaloteiroTeste("Joao");
-		
+
 		CaloteiroDAO dao = new CaloteiroDAO();
 		dao.adiciona(caloteiro);
 		caloteiro.setId(dao.getLista().get(0).getId());
 		Long id = caloteiro.getId();
 		Caloteiro caloteiroDoBanco = dao.getCaloteiro(id);
-		
+
 		assertEquals(dao.getLista().get(0).getId(), caloteiroDoBanco.getId());
 	}
 
-	
+	@Test
+	public void deveAlterarUmCaloteiro() {
+
+		Caloteiro caloteiro = getCaloteiroTeste("Joao");
+
+		CaloteiroDAO dao = new CaloteiroDAO();
+		dao.adiciona(caloteiro);
+		String oldName = caloteiro.getNome();
+		Integer oldDevendo = caloteiro.getDevendo();
+		String oldEmail = caloteiro.getEmail();
+		Long oldId = dao.getLista().get(0).getId();
+		caloteiro.setNome("newName");
+		caloteiro.setEmail("newEmail");
+		caloteiro.setDevendo(oldDevendo - 10);
+		caloteiro.setDataDivida(Calendar.getInstance());
+		caloteiro.setId(oldId);
+		
+		dao.altera(caloteiro);
+
+		assertNotEquals(oldName, caloteiro.getNome());
+		assertNotEquals(oldEmail, caloteiro.getEmail());
+		assertNotEquals(oldDevendo, caloteiro.getDevendo());
+		assertEquals(oldId, caloteiro.getId());
+	}
+
 }
