37a9a55d369d9f4835c1c274fda103c1b4b694af
Davi Leitão <davi.leitao@hotmail.com>
2016-12-28 21:22:10 -0300

    Adição dos métodos setUp() e tearDown() no deveAdicionarUmCaloteiroAceitacao. Falta implementar o rollback na transação com o banco que não funcionou.

--- test-aceitacao/br/com/triadworks/javaweb/selenium/CaloteiroDAOTestAceitacao.java
+++ test-aceitacao/br/com/triadworks/javaweb/selenium/CaloteiroDAOTestAceitacao.java
@@ -1,46 +1,86 @@
 package br.com.triadworks.javaweb.selenium;
 
+import java.sql.SQLException;
+import java.sql.Savepoint;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
+import br.com.triadworks.javaweb.dao.ConnectionFactory;
+
 public class CaloteiroDAOTestAceitacao {
 	
+	private java.sql.Connection connection;
+	private static Long tempoDeEspera = new Long(300);
+	
+	@Before
+	public void setUp() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
-	public void deveAdicionarUmCaloteiroAceitacao() {
+	public void deveAdicionarUmCaloteiroAceitacao() throws SQLException {
+		
+		Savepoint savepoint1 = connection.setSavepoint();
+		try {
+			connection.setAutoCommit(false);
+			
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 		Thread thread = new Thread();
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("http://localhost:8080/calote-web/adicionaCaloteiro.jsp");
 		
 		WebElement campoDeTextoDoNome = driver.findElement(By.name("nome"));
-		campoDeTextoDoNome.sendKeys("Davi Gadelha Leitao");
+		campoDeTextoDoNome.sendKeys("Marcos Fernando Justino");
 		
 		WebElement campoDeTextoDoEmail = driver.findElement(By.name("email"));
 		campoDeTextoDoEmail.sendKeys("davi.leitao@hotmail.com");
 		
 		WebElement campoDeTextoDaDivida = driver.findElement(By.name("devendo"));
-		campoDeTextoDaDivida.sendKeys("300");
+		campoDeTextoDaDivida.sendKeys("800");
 		
 		WebElement campoDeTextoDaData = driver.findElement(By.name("dataDivida"));
 		campoDeTextoDaData.sendKeys("28/12/2016");
 		
-		wait(thread, new Long(2000));
+		wait(thread, new Long(tempoDeEspera));
 		
 		campoDeTextoDoNome.submit();
+		try {
+			connection.commit();
+			connection.rollback(savepoint1);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
-		wait(thread, new Long(2000));
+		wait(thread, new Long(tempoDeEspera));
 		
 		WebElement menuListaCaloteiros = driver.findElement(By.id("lista-caloteiro-menu"));
 		menuListaCaloteiros.click();
 		
 	}
-
+	
 	private void wait(Thread thread, Long time) {
 		try {
 			thread.sleep(time);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
